4287fbf05328e70135854285e61db8de9c7a710f
Ethan Eade <services@ethaneade.com>
2011-07-27 00:25:01 -0700

    * Factor out boilerplate * Add comments

--- quartic.h
+++ quartic.h
@@ -1,22 +1,28 @@
-#ifndef QUARTIC_H
-#define QUARTIC_H
+#ifndef MATHBITS_QUARTIC_H
+#define MATHBITS_QUARTIC_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-    
+#include <mathbits/cpp.h>
+
+BEGIN_C_DECLS;
+
+// Returns one real root of the cubic x^3 + Ax + B
 double real_depressed_cubic_root(double A, double B);
 
+// Returns one real root of the cubic x^3 + Bx^2 + Cx + D
 double real_cubic_root(double B, double C, double D);
 
+// Find all real roots of the cubic x^3 + Bx^2 + Cx + D
+// Stores up to 3 abscissas in roots[].
+// Returns number of roots found.
 int real_cubic_roots(double B, double C, double D,
                      double roots[]);
 
+// Find all real roots of the quartic x^4 + Bx^3 + Cx^2 + Dx + E
+// Stores up to 4 abscissas in roots[].
+// Returns number of roots found, or -1 on numerical error.
 int real_quartic_roots(double B, double C, double D, double E,
                        double roots[]);
 
-#ifdef __cplusplus
-}
-#endif
-    
+END_C_DECLS;
+
 #endif
